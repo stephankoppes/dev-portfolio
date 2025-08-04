69447869872c31aef64b59dfba904d3ded85f1e7
Stephan-Koppes <stephan.koppes@bejo.nl>
2025-08-04 21:39:50 +0200

    Setup Experience table

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+    interface ButtonProps {
+        children: Snippet,
+        onclick: ((e: MouseEvent) => void | (() => void))
+        className?: string;
+    }
+    let { children, className, ...props }: ButtonProps = $props();
+</script>
+
+<button class={'btn ${className}'} {...props}>
+    {@render children()}
+</button>
+
+<style>
+    .btn {
+        font-family: 'Inter Tight', sans-serif;
+        font-weight: 500;
+        background-color: black;
+        color: white;
+        font-size: 24px;
+        padding: 14px 40px;
+        border-radius: 10px;
+    }
+</style>
---
+++ src/lib/components/Sections/HeroSection.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import Button from "$components/Button.svelte";
+
+    function onclick() {
+        goto("/#contact-form");
+    }
+</script>
+<section class="hero-section default-margin">
+    <div>Hello! I'm Stephan</div>
+    <div class="underscore mb-m"></div>
+    <h1>I'm a Svelte developer crafting intuitive and performant</h1>
+    <h1 class="dark-grey mb-m">web experience</h1>
+    <Button {onclick}>Let's talk</Button>
+</section>
+
+<style>
+    .hero-section {
+        padding-top: 60px;
+    }
+</style>
